0b03f7a9c43eea5be7083e072d0d4d468d03522d
Susan Winters <susanwinters214@gmail.com>
2022-10-17 20:32:39 -0500

    practice sugar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+           
+            Console.WriteLine(response);
         }
     }
 }
